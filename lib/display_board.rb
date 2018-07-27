471fde83317465fa49114963254e5f8a4b947165
valentine-m@live.fr <valentine-m@live.fr>
2018-07-27 17:52:34 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+row=["   |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "]
 board=[" X ", " O ", " O ", " X ", " X ", " O ", " X ", " X ", "  O "]
 
 def display_board(board)
-  row=["   |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "]
-  puts row[0]
-  puts row[1]
-  puts row[2]
-  puts row[3]
-  puts row[4]
+puts row[0]
+puts row[1]
+puts row[2]
+puts row[3]
+puts row[4]
+puts row[5]
 end
 display_board
