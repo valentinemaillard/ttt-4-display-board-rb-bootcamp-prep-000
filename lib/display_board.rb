4982ae2976d9b8589f827f0db1f778dccee50dcc
valentine-m@live.fr <valentine-m@live.fr>
2018-07-27 17:36:14 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,11 +2,12 @@
 # out the current state.
 board=[" X ", " O ", " O ", " X ", " X ", " O ", " X ", " X ", "  O "]
 
-def display_board(board)
-cell= "   "
-separation= "|"
-cell_row=cell+separation+cell+separation+cell
-line= "-----------"
-return puts cell_row, line, cell_row, line, cell_row
+def display_board
+  row=["   |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "]
+  puts row[0]
+  puts row[1]
+  puts row[2]
+  puts row[3]
+  puts row[4]
 end
 display_board
