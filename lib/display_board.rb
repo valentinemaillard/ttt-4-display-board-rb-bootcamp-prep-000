00a28dab4d0e2da5b890395d60cb78c1887ee8f7
valentine-m@live.fr <valentine-m@live.fr>
2018-07-27 18:44:28 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@
 board=[" X ", " X ", " X ", " X ", " X ", " X ", " X ", " X ", " X "]
 
 def display_board(board)
-  row=["  #{board[0]}  |  #{board[1]}  |  #{board[2]}  ", "-----------", "#{board[3]}|#{board[4]}|#{board[5]}", "-----------", "#{board[6]}|#{board[7]}|#{board[8]}"]
+  row=["  #{board[0]} |  #{board[1]} |  #{board[2]}  ", "-----------", "#{board[3]}|#{board[4]}|#{board[5]}", "-----------", "#{board[6]}|#{board[7]}|#{board[8]}"]
   puts (row[0])
   puts (row[1])
   puts (row[2])
