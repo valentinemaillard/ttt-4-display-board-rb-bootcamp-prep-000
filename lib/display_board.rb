06aa5f179901954f41c404849c76401ec950f090
valentine-m@live.fr <valentine-m@live.fr>
2018-07-27 17:23:22 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board=[""]
+
+def display_board
+cell= "   "
+separation= "|"
+cell_row=cell+separation+cell+separation+cell
+line= "-----------"
+return puts cell_row, line, cell_row, line, cell_row
+end
+display_board
