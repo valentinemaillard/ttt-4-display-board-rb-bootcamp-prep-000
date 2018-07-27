69baf679464443aebb4ed737eecf702639150ee9
valentine-m@live.fr <valentine-m@live.fr>
2018-07-27 18:13:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 
 row=["   |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "]
-board=[" X ", " O ", " O ", " X ", " X ", " O ", " X ", " X ", "  O "]
+board=[" X ", " O ", " O ", " X ", " X ", " O ", " X ", " O ", "  X "]
 
 def display_board(board)
   row=["#{board[0]}|#{board[1]}|#{board[2]}", "-----------", "#{board[3]}|#{board[4]}|#{board[5]}", "-----------", "#{board[6]}|#{board[7]}|#{board[8]}"]
